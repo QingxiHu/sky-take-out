703449daba1d6ceeda267328eac28d63b338a65c
QingxiHu <470544301h@gmail.com>
2023-08-01 12:45:39 +0800

    添加员工功能模块

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -24,4 +27,18 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    @ExceptionHandler
+    public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException exception) {
+
+        String message = exception.getMessage();
+        if (message.contains("Duplicate entry")) {
+            String[] strings = message.split(" ");
+            String username = strings[2];
+            message = username + MessageConstant.ALREADY_EXISTS;
+            return Result.error(message);
+        }else {
+            return Result.error("未知错误，请联系管理员");
+        }
+
+    }
 }
