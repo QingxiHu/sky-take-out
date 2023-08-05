59529f0c9cf535a263dc69ce3f93cae85369e65f
QingxiHu <470544301h@gmail.com>
2023-08-05 13:00:52 +0800

    修改菜品及其口味功能已实现

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -3,6 +3,7 @@ package com.sky.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.sky.dto.DishDTO;
 import com.sky.entity.Dish;
+import com.sky.vo.DishVO;
 
 import java.util.List;
 
@@ -22,4 +23,15 @@ public interface DishService extends IService<Dish> {
      * 删除菜品
      */
     void removeByIds(List<Long> ids);
+
+    /**
+     * 根据Id获取菜品及口味
+     */
+    DishVO getWithFlavor(long id);
+
+    /**
+     * 修改菜品及其口味
+     * @param dishDTO
+     */
+    void updateWithFlavor(DishDTO dishDTO);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -14,6 +14,8 @@ import com.sky.mapper.DishMapper;
 import com.sky.service.DishFlavorService;
 import com.sky.service.DishService;
 import com.sky.service.SetmealDishService;
+import com.sky.vo.DishVO;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author Administrator
@@ -84,4 +87,43 @@ public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements Di
 
         super.removeByIds(ids);
     }
+
+    /**
+     * 查菜品和口味
+     * @param id
+     * @return
+     */
+    @Override
+    public DishVO getWithFlavor(long id) {
+        Dish dish = super.getById(id);
+
+        LambdaQueryWrapper<DishFlavor> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(DishFlavor::getDishId, id);
+
+        // 口味集合
+        List<DishFlavor> list = dishFlavorService.list(wrapper);
+
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish, dishVO);
+        dishVO.setFlavors(list);
+        return dishVO;
+    }
+
+    /**
+     * 修改菜品及其口味
+     * @param dishDTO
+     */
+    @Override
+    public void updateWithFlavor(DishDTO dishDTO) {
+        Dish dish = new Dish();
+        BeanUtils.copyProperties(dishDTO, dish);
+        super.updateById(dish);
+
+        // 口味数据修改
+        List<DishFlavor> flavors = dishDTO.getFlavors();
+        dishFlavorService.remove(null);
+        flavors.forEach(flavor -> flavor.setDishId(dishDTO.getId()));
+        dishFlavorService.saveBatch(flavors);
+    }
+
 }
